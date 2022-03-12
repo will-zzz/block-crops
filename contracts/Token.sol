81a6bc247659d831e5c4441b27d8f01cf3a8bb45
cheb <ths2238@towerhill.org>
2022-03-11 22:52:11 -0500

    crazy changes

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,11 +11,13 @@ contract CropFarm is ERC1155, ERC1155Supply {
     uint256 public constant MELON = 2;
     uint256 public constant PUMPKIN = 3;
 
-    constructor() public ERC1155("https://game.example/api/item/{id}.json") {
+    constructor() ERC1155("https://game.example/api/item/{id}.json") {
         _mint(msg.sender, WHEAT, 10**18, "");
         _mint(msg.sender, CARROT, 10**27, "");
         _mint(msg.sender, MELON, 1, "");
         _mint(msg.sender, PUMPKIN, 10**9, "");
+
+        _balances[0][msg.sender].totalBalance = 10**18;
     }
 
     struct Balance {
@@ -26,7 +28,8 @@ contract CropFarm is ERC1155, ERC1155Supply {
     // All normal mint / burn / transfer will read from totalBalance
     // during staking, you move quantities away from totalBalance to stakedBalance
 
-    mapping( uint => mapping( address => Balance ) ) internal _balances;
+    mapping(uint256 => mapping(address => Balance)) internal _balances;
+
     //       ^ crop id          ^ user      ^ amount
 
     function _safeTransferFrom(
@@ -35,26 +38,49 @@ contract CropFarm is ERC1155, ERC1155Supply {
         uint256 id,
         uint256 amount,
         bytes memory data
-    ) internal override {
+    ) internal virtual override {
         require(to != address(0), "ERC1155: transfer to the zero address");
         //extra stuff
 
         uint256 fromBalance = _balances[id][from].totalBalance;
-        require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
-        unchecked {
-            _balances[id][from].totalBalance = fromBalance - amount;
-        }
+        require(
+            fromBalance >= amount,
+            "ERC1155: insufficient balance for transfer"
+        );
+        fromBalance = fromBalance - amount;
+        // update
         _balances[id][to].totalBalance += amount;
+        _balances[id][from].totalBalance = fromBalance;
 
-        //extra stuff
+        // then calls inherited safeTransferFrom
+        super._safeTransferFrom(from, to, id, amount, data);
+    }
+
+    function viewValues(
+        address _account,
+        uint256 _id,
+        uint256 _member
+    ) public view returns (uint256) {
+        if (_member == 0) {
+            return _balances[_id][_account].totalBalance;
+        } else if (_member == 1) {
+            return _balances[_id][_account].stakedBalance;
+        } else if (_member == 2) {
+            return _balances[_id][_account].stakeDate;
+        } else {
+            return 0;
+        }
     }
 
     // The following functions are overrides required by Solidity. BASICALLY need this bc of ERC1155Supply
-
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override(ERC1155, ERC1155Supply) {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,13 +1,54 @@
 const { expect } = require("chai");
 
 describe("Token contract", function () {
-  it("Assigns right number of crops", async function () {
-    const [owner] = await ethers.getSigners();
+  let Farm;
+  let farm;
+  let owner;
+  let addr1;
+  let addr2;
 
-    const Farm = await ethers.getContractFactory("CropFarm");
-    const farm = await Farm.deploy();
+  beforeEach(async function () {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    Farm = await ethers.getContractFactory("CropFarm");
+    farm = await Farm.deploy();
+  });
 
-    let ownerBalance = await farm.balanceOf(owner.address, 0);
-    expect(await farm.totalSupply(0)).to.equal(ownerBalance);
+  describe("Deployment", function () {
+    it("Assigns right number of crops", async function () {
+      for (let i = 0; i <= 3; i++) {
+        let ownerBalance = await farm.balanceOf(owner.address, i);
+        expect(await farm.totalSupply(i)).to.equal(ownerBalance);
+        // console.log("There are " + ownerBalance.toString() + " tokens with ID " + i);
+      }
+    });
+
+    it("Transfers crops", async function () {
+      await farm
+        .connect(owner)
+        .safeTransferFrom(owner.address, addr1.address, 0, 50, 0x00);
+      expect(await farm.balanceOf(addr1.address, 0)).to.equal(50);
+      // // TOKEN / MAPPING VALUES AFTER TRANSFER
+      // console.log(
+      //   "Owner has " +
+      //     (await farm.balanceOf(owner.address, 0)) +
+      //     " tokens of ID 0 on blockchain."
+      // );
+      // console.log(
+      //   (
+      //     "Owner has " + (await farm.viewValues(owner.address, 0, 0))
+      //   ).toString() + " tokens of ID 0 in mapping."
+      // );
+      // console.log(
+      //   "addr1 has " +
+      //     (await farm.balanceOf(addr1.address, 0)) +
+      //     " tokens of ID 0 on blockchain."
+      // );
+      // console.log(
+      //   (
+      //     "addr1 has " + (await farm.viewValues(addr1.address, 0, 0))
+      //   ).toString() + " tokens of ID 0 in mapping."
+      // );
+    });
+    it("bruhs");
   });
 });
