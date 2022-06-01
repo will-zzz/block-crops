bfd91dba9928843315551f2097f1bd3ea31548fb
cheb <ths2238@towerhill.org>
2022-06-01 19:49:50 -0400

    LFGGGGGG

--- newFrontend/src/components/plot.js
+++ newFrontend/src/components/plot.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import data from "../data";
 
 export default function Plot(props) {
-  const [image, setImage] = useState(null);
+  // const [image, setImage] = useState(null);
+  const [cropClass, setCropClass] = useState("empty");
   const [plotState, setPlotState] = useState(null);
   const [cropName, setCropName] = useState(null);
 
@@ -10,22 +11,24 @@ export default function Plot(props) {
     // if no crop in plot
     if (plotState === null && props.cropName) {
       setCropName(props.cropName);
-      setImage(process.env.PUBLIC_URL + "/images/" + props.cropName + "_5.png");
+      // setImage(process.env.PUBLIC_URL + "/images/" + props.cropName + "_5.png");
+      setCropClass("crops--tomato0");
       setPlotState("growing");
       setTimeout(function () {
         setPlotState("grown");
       }, 3000);
       // Crop done growing
     } else if (plotState === "grown") {
-      setImage(null);
+      // setImage(null);
       setCropName(null);
       setPlotState(null);
     }
   }
 
   return (
-    <div>
-      <img className="plot" src={image} onClick={handleClick} />
-    </div>
+    // <div>
+    /* <img className="plot" src={image} onClick={handleClick} /> */
+    <span className={cropClass} onClick={handleClick} />
+    // </div>
   );
 }
